669ae32881590112141f0dc687eb23768699d56e
Bruno Tinen <bruno@localhost.localdomain>
2017-06-20 22:29:38 -0300

    Ke hue count

--- aes_classifier/attributes/ke_hue_count.py
+++ aes_classifier/attributes/ke_hue_count.py
@@ -1,5 +1,6 @@
 from ml_buff.models.base_feature_record import BaseFeatureRecord
 import cv2
+import numpy as np
 
 IMAGE_PATH = '/home/bruno/Downloads/images4AVA'
 
@@ -9,12 +10,20 @@ class KeHueCount(BaseFeatureRecord):
         if (image is None):
             return [0]
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        channels = cv2.split(hsv)
-        h_hist = cv2.calcHist(hsv, [0], None, [20], [0, 180])
-        s_hist = cv2.calcHist(hsv, [1], None, [20], [0, 256])
-        cv2.normalize(s_hist, s_hist)
-        v_hist = cv2.calcHist(hsv, [2], None, [20], [0, 256])
-        cv2.normalize(v_hist, v_hist)
+        histogram = [0] * 20 #20 bin histogram
+        for i in range(0, hsv.size):
+            for j in range(0, hsv[i].size):
+                if hsv[i][j][1] >= 51 and hsv[i][j][2] >= 38 and hsv[i][j] <= 243:
+                    histogram[hsv[i][j][0]%20] = histogram[hsv[i][j][0]%20] + 1
 
-        return cv2.normalize(histogram, histogram)
+        max = np.amax(histogram)
+        alpha = 0.2
+
+        threshold = max * alpha
+
+        N = 0
+        for i in range(0, len(histogram)):
+            if histogram[i] > threshold:
+                N = N + 1
+        return 20 - N
 
