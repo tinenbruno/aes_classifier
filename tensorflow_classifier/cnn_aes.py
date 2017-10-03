f3de8a43d98515f39b3784a94bd71ce001422721
Bruno Tinen <bruno@localhost.localdomain>
2017-10-02 21:39:51 -0300

    Added basic cnn model

---
+++ tensorflow_classifier/cnn_aes.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Imports
+import numpy as np
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+if __name__ == "__main__":
+    tf.app.run()
+
+def main(unused_argv):
+    # Load training and eval data
+
