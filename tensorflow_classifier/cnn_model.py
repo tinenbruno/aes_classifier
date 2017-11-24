0dba53a92003d60d00dc349afc870efb60460ae7
Bruno Tinen <tinen.bruno@gmail.com>
2017-11-24 01:27:05 -0200

    Removed unused code

--- tensorflow_classifier/cnn_model.py
+++ tensorflow_classifier/cnn_model.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 # Imports
 import numpy as np
 import tensorflow as tf
@@ -13,12 +9,10 @@ def cnn_model_fn(features, labels, mode):
     image_width = 200
     image_height = 200
     channels = 3
-    print(features, flush=True)
 
     input_layer = tf.reshape(
             features,
             [batch_size, image_width, image_height, channels])
-    print(input_layer, flush=True)
 
     # Convolutional Layer #1
     conv1 = tf.layers.conv2d(
@@ -27,11 +21,9 @@ def cnn_model_fn(features, labels, mode):
         kernel_size=[5, 5],
         padding="same",
         activation=tf.nn.relu)
-    print(conv1, flush=True)
 
     # Pooling Layer #1
     pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-    print(pool1, flush=True)
 
     # Convolutional Layer #2 and Pooling Layer #2
     conv2 = tf.layers.conv2d(
@@ -41,7 +33,6 @@ def cnn_model_fn(features, labels, mode):
         padding="same",
         activation=tf.nn.relu)
     pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-    print(pool2, flush=True)
 
     # Dense Layer
     pool2_flat = tf.reshape(pool2, [-1, 50 * 50 * 64])
