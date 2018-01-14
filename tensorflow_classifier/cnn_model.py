022eadeff760732cbfe40f57527bee94394c601d
Bruno Tinen <tinen.bruno@gmail.com>
2018-01-14 18:42:03 -0200

    Simplified cnn model

--- tensorflow_classifier/cnn_model.py
+++ tensorflow_classifier/cnn_model.py
@@ -2,6 +2,66 @@
 import numpy as np
 import tensorflow as tf
 
+def simplified_cnn_model_fn(features, labels, mode):
+    """Model function for CNN."""
+    # Input Layer
+    batch_size = -1  # batch dimension is dynamically computed
+    image_width = 200
+    image_height = 200
+    channels = 3
+
+
+    input_layer = tf.reshape(
+            features,
+            [batch_size, image_width, image_height, channels])
+
+    # Convolutional Layer #1
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=8,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+
+    # Pooling Layer #1
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+    # Dense Layer
+    pool1_flat = tf.reshape(pool1, [-1, 100 * 100 * 8])
+    dense = tf.layers.dense(inputs=pool1_flat, units=1024, activation=tf.nn.relu)
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+    # Logits Layer
+    # Our final layer has 2 units, representing the two final classes
+    logits = tf.layers.dense(inputs=dropout, units=2)
+
+    predictions = _predictions(logits)
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=2)
+    loss = tf.losses.softmax_cross_entropy(
+        onehot_labels=onehot_labels, logits=logits)
+
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"])}
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
 def cnn_model_fn(features, labels, mode):
     """Model function for CNN."""
     # Input Layer
@@ -44,18 +104,13 @@ def cnn_model_fn(features, labels, mode):
     # Our final layer has 2 units, representing the two final classes
     logits = tf.layers.dense(inputs=dropout, units=2)
 
-    predictions = {
-        # Generate predictions (for PREDICT and EVAL mode)
-        "classes": tf.argmax(input=logits, axis=1),
-        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-        # `logging_hook`.
-        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-    }
+    predictions = _predictions(logits)
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
     # Calculate Loss (for both TRAIN and EVAL modes)
+
     onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=2)
     loss = tf.losses.softmax_cross_entropy(
         onehot_labels=onehot_labels, logits=logits)
@@ -74,3 +129,12 @@ def cnn_model_fn(features, labels, mode):
             labels=labels, predictions=predictions["classes"])}
     return tf.estimator.EstimatorSpec(
         mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+def _predictions(output_layer):
+    return {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=output_layer, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.softmax(output_layer, name="softmax_tensor")
+    }
